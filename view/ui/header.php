055a78dd2a45d1b59fce4e45e14c0ae9119c8562
Vivien60 <phedras.ch@gmail.com>
2025-10-15 17:09:37 +0200

    Fixes: routes, url and assets folder structure Fix some routes and add url in header Move some folders into img folder: icons, logo and books

--- view/ui/header.php
+++ view/ui/header.php
@@ -7,15 +7,15 @@ return
 
     <<<HTML
 
-<nav><a href="#"><img alt="" src="assets/logo/extended.svg"></a>
+<nav><a href="#"><img alt="" src="assets/img/logo/extended.svg"></a>
     <ul>
-        <li><a href="#">Accueil</a></li>
+        <li><a href="?action=home">Accueil</a></li>
         <li><a href="#">Nos livres à l’échange</a></li>
     </ul>
     <ul>
-        <li><a href="#"><i aria-hidden="true" class="messagerie-icon"></i><span>Messagerie</span><span>1</span></a></li>
-        <li><a href="#"><i aria-hidden="true" class="account-icon"></i>Mon compte</a></li>
-        <li><a href="#">Connexion</a></li>
+        <li><a href="?action=messagerie"><i aria-hidden="true" class="messagerie-icon"></i><span>Messagerie</span><span>1</span></a></li>
+        <li><a href="?action=edit-profile-form"><i aria-hidden="true" class="account-icon"></i>Mon compte</a></li>
+        <li><a href="?action=sign-in">Connexion</a></li>
     </ul>
 </nav>
 
