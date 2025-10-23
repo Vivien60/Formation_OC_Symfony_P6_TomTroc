89ffcad81ebbd86124cd7620a67f93a087fb2148
Vivien60 <phedras.ch@gmail.com>
2025-10-23 22:46:48 +0200

    Feat/fix(ui) : book copy edit css - wip Positions and sizes globally
    #69

--- view/ui/bookCopyEdit.php
+++ view/ui/bookCopyEdit.php
@@ -6,36 +6,38 @@ use view\templates\AbstractHtmlTemplate;
  */
 
 return
-<<<HTML
+    <<<HTML
 <nav><a>&larr; retour</a></nav>
 <h1>Modifier les informations</h1>
-<div>
-    <label>
-        <div><img src="assets/img/books/book02.jpg"></div>
-        <a>Modifier la photo</a>
+<div class="container container--book-copy-edit">
+    <label class="photo-upload">
+        Photo
+        <div class="photo-upload__preview"><img class="photo-upload__image" src="assets/img/books/book02.jpg"></div>
+        <a class="photo-upload__action">Modifier la photo</a>
     </label>
-</div>
-<div>
-    <form name="book-copy-edit" method="post" action="?action=book-copy-save&id=%IdLivre%">
-        <label>
-            Titre
-            <input name="title" type="text" value="%TitreDuLivre%">
-        </label>
-        <label>
-            Auteur
-            <input name="auteur" type="text" value="%AuteurDuLivre%">
-        </label>
-        <label>
-            Commentaire
-            <textarea name="description">%DescriptionDuLivreParLeUser%</textarea>
-        </label>
-        <label>
-            <select name="availabilityStatus">
-                <option value="1"">disponible</option>
-                <option value="0">Indisponible</option>
-            </select>
-        </label>
-        <input type="submit">
-    </form>
+    <div class="container container--book-copy-edit container--form">
+        <form class="form form--book-edit" name="book-copy-edit" method="post" action="?action=book-copy-save&id=%IdLivre%">
+            <label class="form__label">
+                Titre
+                <input class="form__field" name="title" type="text" value="%TitreDuLivre%">
+            </label>
+            <label class="form__label">
+                Auteur
+                <input class="form__field" name="auteur" type="text" value="%AuteurDuLivre%">
+            </label>
+            <label class="form__label">
+                Commentaire
+                <textarea class="form__field form__field--textarea" name="description">%DescriptionDuLivreParLeUser%</textarea>
+            </label>
+            <label class="form__label">
+                Disponibilité
+                <select class="form__field form__field--select" name="availabilityStatus">
+                    <option value="1"">disponible</option>
+                    <option value="0">Indisponible</option>
+                </select>
+            </label>
+            <input class="bigButton" type="submit">
+        </form>
+    </div>
 </div>
 HTML;
