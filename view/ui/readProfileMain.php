09027dafe7e51c5791cd5e308f14fb3930e69911
Vivien60 <phedras.ch@gmail.com>
2025-11-28 15:50:39 +0100

    Fix: affichage card user FIx problem with user card which prevents it to be displayed

--- view/ui/readProfileMain.php
+++ view/ui/readProfileMain.php
@@ -11,13 +11,14 @@ use view\templates\ReadProfile;
 $writeMessageLink = '<a class="bigButton bigButton--light bigButon--max-size" href="?action=write-message">Ecrire un message</a>';
 $bigUserCard = require __DIR__.'/component/cardUserBig.php';
 $htmlBigUserCard = sprintf(
-        $bigUserCard,
-        '',
-        $this->user->id,
-        $this->e($this->user->username),
-        Utils::convertDateToFrenchFormat($this->user->createdAt),
-        count($this->user->library),
-        $writeMessageLink
+    $bigUserCard,
+    '',
+    $this->user->id,
+    $this->e($this->user->username),
+    Utils::convertDateToFrenchFormat($this->user->createdAt),
+    count($this->user->library),
+    $writeMessageLink,
+    $this->user->avatar,
 );
 
 $libraryUserHTML = '';
